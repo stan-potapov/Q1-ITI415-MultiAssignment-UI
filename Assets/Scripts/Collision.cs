6e1e042f4cf2229376046cc61214118c8ca7ce4d
stan-potapov <stan.potapov98@gmail.com>
2020-05-31 23:52:47 -0400

    did the thing

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Collision : MonoBehaviour
+{
+    public Text countText;
+    public Text winText;
+    public int totalCount;
+
+    private Rigidbody rb;
+    private int count;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Pickup")) {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText() {
+        countText.text = "Coin Count: " + count.ToString();
+        if (count == totalCount) {
+            winText.text = "You Win!";
+        }
+    }
+}
